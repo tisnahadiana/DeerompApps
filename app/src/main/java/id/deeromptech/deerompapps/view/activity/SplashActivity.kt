4c2476cba31b1f37a0a09da9fb2c85ce98bf934d
ACER <tisnahadiana@gmail.com>
2024-02-13 11:13:27 +0700

    Initial commit

---
+++ app/src/main/java/id/deeromptech/deerompapps/view/activity/SplashActivity.kt
@@ -0,0 +1,12 @@
+package id.deeromptech.deerompapps.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.deeromptech.deerompapps.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
